ee5a45fc9e8240296c2d96b5c06555ac3543f213
nico <nico.lagoutte@gmail.com>
2017-07-03 19:26:03 +0100

    basic restructure and use the required filters.

--- main.js
+++ main.js
@@ -1,18 +1,45 @@
 "use strict"
 
+const qs = require('qs')
+const moment = require('moment')
 
 const GITHUB_API = "https://api.github.com"
 
-async function main() {
-    const resultElement = document.querySelector('#results')
-
-    const url = `${GITHUB_API}/search/repositories?q=` + encodeURIComponent('Vue.js')
+async function searchGithub(search) {
+    const searchParameters = {
+        q: 'Vue.js',
+        sort: 'stars',
+        pushed: '>' + moment().subtract(10, 'days').format('YYYY-MM-DD'),
+        order: 'desc'
+    }
+    const url = `${GITHUB_API}/search/repositories?${qs.stringify(searchParameters)}`
+    console.log(url)
     const response = await fetch(url)
-    const results = await response.json()
+    return await response.json()
+}
 
+function renderResults(results) {
+    const resultElement = document.querySelector('#results')
+    //TODO use handlebar
     resultElement.innerHTML = JSON.stringify(results)
 }
 
+function indicateError() {
+    alert("System error") //TODO this should be rendered nicely in html
+}
+
+async function runSearch() {
+    try {
+
+        const searchResults = await searchGithub("Vue.js")
+        renderResults(searchResults)
+
+    } catch(e) {
+        console.error(e)
+        indicateError()
+    }
+}
+
 document.addEventListener('DOMContentLoaded', function() {
-    main()
+    runSearch()
 })
