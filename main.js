37bc86fd9b05c38c8fea83f4f33356f8a69bd954
nico <nico.lagoutte@gmail.com>
2017-07-03 19:10:08 +0100

    Fetch hardcoded results from github and output in the page, ugly

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-alert("browserify works fine")
+"use strict"
+
+
+const GITHUB_API = "https://api.github.com"
+
+async function main() {
+    const resultElement = document.querySelector('#results')
+
+    const url = `${GITHUB_API}/search/repositories?q=` + encodeURIComponent('Vue.js')
+    const response = await fetch(url)
+    const results = await response.json()
+
+    resultElement.innerHTML = JSON.stringify(results)
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    main()
+})
